d2aea4bd1bf2ca22a00f7403038ace2c2fee2420
Alok Singh <alok.sabran@gmail.com>
2020-09-22 15:06:57 +0530

    Changed text

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <a href="https://google.com">
-            <b> Welcome to Google. </b>
-            </a>
-        </p>
+        <p>Changed text !! On Feature Branch.</p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
